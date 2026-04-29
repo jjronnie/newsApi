0e5ad3b5c0c4cb6bbf1c29cef91f68b30a02ca78
JRonnie <ronaldjjuuko7@gmail.com>
2026-04-29 21:51:57 +0300

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             WebsiteSeeder::class,
+            ArticleTopicsSeeder::class,
+            AiInstructionsSeeder::class,
+
         ]);
     }
 }
